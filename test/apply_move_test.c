e5d5455d73cdbf17af8354c1f2d7bbf0bb428cb8
Davide Cazzin <cazzindavide@gmail.com>
2020-11-28 18:50:58 +0100

    Add test in apply_move_test.c

--- test/apply_move_test.c
+++ test/apply_move_test.c
@@ -1,7 +1,33 @@
 #include "../src/logic.h"
-#include "../src/utility.h"
 
-void test_apply_move1() {
+void test_apply_move_eat() {
+    Piece *board = calloc(ROWS * COLUMNS, sizeof(Piece));
+    Pos piecePos1, piecePos2, newPos1;
+    Move move;
+    Piece expectedPiece, actualPiece;
+    bool expected, actual;
+
+    piecePos1 = initialize_pos(2,2);
+    piecePos2 = initialize_pos(3,3);
+
+    board[get_index_from_pos(piecePos1)] = initialize_piece(WHITE, UNDEFINED, UNDEFINED, false, 1);
+    board[get_index_from_pos(piecePos2)] = initialize_piece(BLACK, UNDEFINED, UNDEFINED, false, 1);
+
+    newPos1 = initialize_pos(4,4);
+    move = initialize_move(piecePos1, newPos1);
+
+    expected = true;
+    actual = apply_move(board, WHITE, move);
+    assert(actual == expected);
+
+    expectedPiece = initialize_piece(WHITE, BLACK, UNDEFINED, false, 1);
+    actualPiece = board[get_index_from_pos(newPos1)];
+    assert(is_piece_equal(expectedPiece, actualPiece));
+
+    assert(is_piece_null(board[get_index_from_pos(piecePos1)]) == true);
+}
+
+void test_apply_move_liberate_prisoner() {
     /* http://www.lasca.org/show?6V6
     http://www.lasca.org/pictures/nx/3ii.gif
     http://www.lasca.org/pictures/nx/3iii.gif
@@ -9,9 +35,9 @@ void test_apply_move1() {
 
     Piece *board = calloc(ROWS * COLUMNS, sizeof(Piece));
     Pos piecePos1, piecePos2, piecePos3;
-    Pos newPos1, newPos2;
-    Move move1, move2;
-    Piece actualPiece, expectedPiece;
+    Pos newPos1, newPos3;
+    Move move3, move1;
+    Piece expectedPiece, actualPiece;
     bool expected, actual;
 
     piecePos1 = initialize_pos(2, 0);
@@ -22,28 +48,30 @@ void test_apply_move1() {
     board[get_index_from_pos(piecePos2)] = initialize_piece(WHITE, UNDEFINED, UNDEFINED, false, 1);
     board[get_index_from_pos(piecePos3)] = initialize_piece(BLACK, WHITE, UNDEFINED, false, 2);
 
-    newPos1 = initialize_pos(1, 1);
-    newPos2 = initialize_pos(0, 2);
+    newPos3 = initialize_pos(1, 1);
+    newPos1 = initialize_pos(0, 2);
 
-    move1 = initialize_move(piecePos3, newPos1);
+    move3 = initialize_move(piecePos3, newPos3);
 
     expected = true;
-    actual = apply_move(board, BLACK, move1);
+    actual = apply_move(board, BLACK, move3);
 
     assert(expected == actual);
 
-    move2 = initialize_move(piecePos1, newPos2);
+    move1 = initialize_move(piecePos1, newPos1);
 
     expected = true;
-    actual = apply_move(board, WHITE, move2);
+    actual = apply_move(board, WHITE, move1);
+
     assert(expected == actual);
 
-    actualPiece = board[get_index_from_pos(newPos2)];
+    actualPiece = board[get_index_from_pos(newPos1)];
     expectedPiece = initialize_piece(WHITE, BLACK, UNDEFINED, false, 2);
     
     assert(is_piece_equal(expectedPiece, actualPiece));
 }
 
 int main() {
-    test_apply_move1();
+    test_apply_move_eat();
+    test_apply_move_liberate_prisoner();
 }
